80886356b10af309e889f4c2cabf190669c28a98
Jaehak Kim <ministove3yo@gmail.com>
2022-11-25 20:28:52 -0500

    login done

--- src/component/login/login.tsx
+++ src/component/login/login.tsx
@@ -7,16 +7,25 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  Dimensions,
   View,
+  TouchableOpacity,
+  Image,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import SvgUri, { Path } from "react-native-svg";
+
 import {
   useFonts,
   Inter_900Black,
   Inter_600SemiBold,
   Inter_400Regular,
 } from "@expo-google-fonts/inter";
-import React from "react";
+
+const { width, height } = Dimensions.get("window");
+
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 
 function Upper() {
   const stylesUpper = StyleSheet.create({
@@ -25,11 +34,20 @@ function Upper() {
       flexDirection: "column",
       alignItems: "center",
       justifyContent: "center",
+      borderRadius: 30,
+      color: "#121212",
+      margin: 50,
     },
     title: {
       fontFamily: "Inter_900Black",
       color: "white",
       fontSize: 40,
+    },
+    logo: {
+      position: "absolute",
+
+      width: 300,
+      height: 300,
     },
   });
   return (
@@ -79,7 +97,173 @@ function LoginInput() {
       paddingBottom: 13,
       alignSelf: "stretch",
     },
+    button: {
+      position: "absolute",
+      fontFamily: "Inter_400Regular",
+      bottom: "30%",
+      alignItems: "center",
+      backgroundColor: "#1d1d1d",
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      minWidth: 100,
+      borderRadius: 20,
+    },
+    buttonText: {
+      fontSize: 16,
+      color: "#dddddd",
+    },
   });
+  let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+  let waterlooRegex = new RegExp("[a-z0-9]+@uwaterloo.ca");
+  const [sliderState, setSliderState] = useState(0);
+
+  const TypedHandler = (email: string) => {
+    if (email) {
+      if (!regex.test(email)) {
+        setSliderState(3);
+      } else if (!waterlooRegex.test(email)) {
+        setSliderState(4);
+      } else {
+        axios
+          .post("http://35.182.206.211:8000/api/accounts/test/", {
+            email: email,
+          })
+          .then((data: AxiosResponse<boolean>) => {
+            if (data.data) {
+              setSliderState(1);
+            } else {
+              setSliderState(2);
+            }
+          });
+
+        {
+        }
+      }
+    }
+  };
+  const [emailInput, setEmailInput] = useState("");
+  let stateTextButton = [
+    "",
+    <TouchableOpacity
+      style={stylesLoginInput.button}
+      onPress={(e) => {
+        setInputState(1);
+        setSliderState(5);
+      }}
+    >
+      <Text style={stylesLoginInput.buttonText}>Proceed</Text>
+    </TouchableOpacity>,
+    <TouchableOpacity
+      style={stylesLoginInput.button}
+      onPress={(e) => {
+        setInputState(2);
+        setSliderState(6);
+      }}
+    >
+      <Text style={stylesLoginInput.buttonText}>Register</Text>
+    </TouchableOpacity>,
+    "",
+    "",
+    <TouchableOpacity
+      style={stylesLoginInput.button}
+      onPress={(e) => {
+        axios
+          .post("http://35.182.206.211:8000/api/accounts/login/", {
+            email: emailInput,
+            password: password,
+          })
+          .then((data: AxiosResponse<boolean>) => {
+            if (data.data) {
+              setSliderState(1);
+            } else {
+              setSliderState(2);
+            }
+          });
+      }}
+    >
+      <Text style={stylesLoginInput.buttonText}>Log In</Text>
+    </TouchableOpacity>,
+    <TouchableOpacity
+      style={stylesLoginInput.button}
+      onPress={(e) => {
+        axios
+          .post("http://35.182.206.211:8000/api/accounts/registration/", {
+            email: emailInput,
+            password1: password,
+            password2: password1,
+          })
+          .then((data) => {
+            if (data.data["access_token"]) {
+              setSliderState(1);
+            } else {
+              setSliderState(2);
+            }
+          });
+      }}
+    >
+      <Text style={stylesLoginInput.buttonText}>Create Account</Text>
+    </TouchableOpacity>,
+  ];
+  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [inputState, setInputState] = useState(0);
+  let stateInput = [
+    <TextInput
+      placeholderTextColor={"white"}
+      cursorColor={"gray"}
+      spellCheck={false}
+      style={stylesLoginInput.miniText}
+      maxLength={30}
+      keyboardType="email-address"
+      placeholder={"Type here your email to proceed"}
+      contextMenuHidden={true}
+      blurOnSubmit={true}
+      clearTextOnFocus={true}
+      onChangeText={setEmailInput}
+      onEndEditing={(e) => TypedHandler(e.nativeEvent.text)}
+    />,
+    <TextInput
+      placeholderTextColor={"white"}
+      cursorColor={"gray"}
+      spellCheck={false}
+      style={stylesLoginInput.miniText}
+      maxLength={30}
+      keyboardType="visible-password"
+      placeholder={"Type here your password"}
+      contextMenuHidden={true}
+      blurOnSubmit={true}
+      clearTextOnFocus={true}
+      onChangeText={setPassword}
+    />,
+    <View>
+      <TextInput
+        placeholderTextColor={"white"}
+        cursorColor={"gray"}
+        spellCheck={false}
+        style={stylesLoginInput.miniText}
+        maxLength={30}
+        keyboardType="visible-password"
+        placeholder={"Set password here"}
+        contextMenuHidden={true}
+        blurOnSubmit={true}
+        clearTextOnFocus={true}
+        onChangeText={setPassword}
+      />
+      <TextInput
+        placeholderTextColor={"white"}
+        cursorColor={"gray"}
+        spellCheck={false}
+        style={stylesLoginInput.miniText}
+        maxLength={30}
+        keyboardType="visible-password"
+        placeholder={"Confirm password"}
+        contextMenuHidden={true}
+        blurOnSubmit={true}
+        clearTextOnFocus={true}
+        onChangeText={setPassword1}
+      />
+    </View>,
+  ];
 
   return (
     <View style={stylesLoginInput.inputContainer}>
@@ -87,19 +271,9 @@ function LoginInput() {
         <Text style={stylesLoginInput.text}>Welcome</Text>
       </View>
       <View style={stylesLoginInput.emailContainer}>
-        <TextInput
-          placeholderTextColor={"white"}
-          cursorColor={"gray"}
-          spellCheck={false}
-          style={stylesLoginInput.miniText}
-          maxLength={30}
-          keyboardType="email-address"
-          placeholder={"Type here your email to proceed"}
-          contextMenuHidden={true}
-          blurOnSubmit={true}
-          clearTextOnFocus={true}
-        />
+        {stateInput[inputState]}
       </View>
+      {stateTextButton[sliderState]}
     </View>
   );
 }
